a1118c02be69ac62dd1c7ba143887a9cefbc2f6d
DiazMarco2005 <marcoalejandro.diazcastaneda@gmail.com>
2025-02-28 23:16:09 -0600

    javadoc and comments

--- lisp/src/main/java/com/interpeter/Lexer.java
+++ lisp/src/main/java/com/interpeter/Lexer.java
@@ -1,14 +1,35 @@
 package com.interpeter;
 
+/*
+ * This class is responsible for tokenizing the input string.
+ * It checks if the input string is correct and then tokenizes it.
+ * It also checks if the token is a reserved word.
+ * This class was made by Marco Alejandro Díaz Castañeda
+ * Carné 24229
+ * Universidad del Valle de Guatemala
+ * 28/2/2025
+ * Los comentarios fueron hechos con inteligencia artificial 
+ * 
+ */
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
 public class Lexer {
+    
     public Lexer() {
     }
 
+    /**
+     * Checks if a given token is a reserved word in Lisp.
+     *
+     * @param token the token to check
+     * @return true if the token is a reserved word, false otherwise
+     * This class was based on a class made by Chatgpt
+     * It was addapted to the needs of the project by Marco Díaz
+     */
     private static boolean reservedWord(String token) {
         final Set<String> lispReservedWords = new HashSet<>();
         // Special Forms from the Common Lisp standard
@@ -50,9 +71,12 @@ public class Lexer {
         return lispReservedWords.contains(token.toLowerCase());
     }
 
-
-
-
+    /**
+     * Checks if the input string is correct by verifying the balance of parentheses.
+     *
+     * @param input the input string to check
+     * @return true if the input string is correct, false otherwise
+     */
     public boolean isCorrect(String input) {
         ArrayList<String> tokens = new ArrayList<String>();
 
@@ -61,10 +85,7 @@ public class Lexer {
         }
         if (tokens.size() == 0) {
             return false;
-
         }
-
-        
     
         int count = Collections.frequency(tokens, "(");
         int count2 = Collections.frequency(tokens, ")");
@@ -75,11 +96,24 @@ public class Lexer {
         return true;
     }
 
+    /**
+     * Tokenizes the input string into a list of tokens.
+     * Reserved words are converted to uppercase.
+     *
+     * @param input the input string to tokenize
+     * @return a list of tokens, or null if the input string is incorrect
+     */
     public ArrayList<String> tokenize(String input) {
         if (isCorrect(input)) {
             ArrayList<String> tokens = new ArrayList<String>();
             for (String i : input.split(" ")) {
-                tokens.add(i);
+                if(reservedWord(i)){
+                    tokens.add(i.toUpperCase());
+                    // se ponen en mayuscula las palabras reservadas para diferenciarlas de las variables
+                }
+                else{
+                    tokens.add(i);
+                }
             }
             return tokens;
 
